f6e41f5e159889af46f70326ca7aebc0742695a5
Thomas GICQUEL <m.gicquel.thomas@gmail.com>
2020-03-20 22:41:09 +0100

    Initialisation du project + architecture

---
+++ OceanOfCode/Program.cs
@@ -0,0 +1,581 @@
+#define WriteDebug
+
+using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace OceanOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var gameManager = new GameManager();
+            gameManager.InitializeMap();
+
+
+            // Write an action using Console.WriteLine()
+            // To debug: Console.Error.WriteLine("Debug messages...");
+            Console.WriteLine("7 7");
+
+            // game loop
+            while (true)
+            {
+                gameManager.SetPlayersInformations();
+                string sonarResult = Helpers.ReadLine();
+                string opponentOrders = Helpers.ReadLine();
+
+                gameManager.WritePosition();
+                gameManager.Play();
+
+                // Write an action using Console.WriteLine()
+                // To debug: Console.Error.WriteLine("Debug messages...");
+
+
+                //Console.WriteLine("MOVE N TORPEDO");
+            }
+        }
+    }
+
+    #region Models
+    public class Map
+    {
+        /// <summary>
+        /// La largeur de la carte
+        /// </summary>
+        public int Width { get; set; }
+        /// <summary>
+        /// La hauteur de la carte
+        /// </summary>
+        public int Height { get; set; }
+
+        /// <summary>
+        /// La configuration de la carte
+        /// </summary>
+        public List<List<MapCell>> MapConfiguration { get; set; } = new List<List<MapCell>>();
+    }
+
+    public class MapCell
+    {
+        /// <summary>
+        /// La position de la cellule
+        /// </summary>
+        public Position Position { get; set; }
+        /// <summary>
+        /// Le caractère qui correspond à la cellule
+        /// </summary>
+        public char Cell { get; set; }
+
+        /// <summary>
+        /// On ne doit pas se déplacer sur une cellule qu'on a déjà visité
+        /// Sauf dans le cas où on refait surface
+        /// </summary>
+        public bool Visited { get; set; } = false;
+        /// <summary>
+        /// Le type de cellule
+        /// </summary>
+        public CellType CellType => Cell.ToCellType();
+    }
+
+    public class Position
+    {
+        public int X { get; set; } = -1;
+        public int Y { get; set; } = -1;
+
+        /// <summary>
+        /// Permet d'obtenir la section de la position
+        /// </summary>
+        public int Section => this.ToSection();
+        public override string ToString()
+        {
+            return $"{{{X}:{Y}}}";
+        }
+
+    }
+
+    public class Player
+    {
+        /// <summary>
+        /// Identifiant du joueur
+        /// Le joueur avec l'identifiant 0 commande
+        /// </summary>
+        public int PlayerId { get; set; }
+        /// <summary>
+        /// Défini s'il s'agit de moi ou pas
+        /// </summary>
+        public PlayerType PlayerType { get; set; }
+
+        /// <summary>
+        /// La position du joueur
+        /// </summary>
+        public Position Position { get; set; } = new Position();
+
+        /// <summary>
+        /// Le nombre de point de vie
+        /// on démarre avec 6 points de vie
+        /// </summary>
+        public int HealthPoint { get; set; } = 6;
+
+        /// <summary>
+        /// Les 'armes' disponibles
+        /// </summary>
+        public List<Device> Devices { get; set; } = new List<Device> {
+            new Torpedo { },
+            new Sonar {  },
+            new Silence { },
+            new Mine {  }
+        };
+
+        public List<Direction> LastMoves = new List<Direction>();
+
+        #region Helpers
+        public Torpedo Torpedo => (Torpedo)Device(DeviceType.Torpedo);
+        public Sonar Sonar => (Sonar)Device(DeviceType.Sonar);
+        public Mine Mine => (Mine)Device(DeviceType.Mine);
+        public Silence Silence => (Silence)Device(DeviceType.Silence);
+        protected Device Device(DeviceType DeviceType) => Devices.First(d => d.DeviceType == DeviceType);
+
+        /// <summary>
+        /// Défini si le joueur est le premier à jouer
+        /// </summary>
+        public bool First => PlayerId == 0;
+        public bool ImOnTop => Position.Y <= 7;
+        public bool ImOnLeft => Position.X <= 7;
+        #endregion
+
+    }
+
+    public abstract class Device
+    {
+        /// <summary>
+        /// Le type d'arme
+        /// </summary>
+        public DeviceType DeviceType { get; set; }
+        /// <summary>
+        /// Le temps à attendre avant réutilisation (disponibilité)
+        /// </summary>
+        public int Couldown { get; set; }
+
+        public abstract bool CanUse();
+        public abstract string Use();
+    }
+
+    public class Torpedo
+        : Device
+    {
+        public Torpedo()
+        {
+            DeviceType = DeviceType.Torpedo;
+        }
+
+        public override bool CanUse()
+        {
+            return Couldown == 0;
+        }
+
+        public override string Use()
+        {
+            return "TORNEDO {0} {1}";
+        }
+    }
+
+    public class Sonar
+    : Device
+    {
+        public Sonar()
+        {
+            DeviceType = DeviceType.Sonar;
+        }
+
+        public override bool CanUse()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string Use()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Mine
+    : Device
+    {
+        public Mine()
+        {
+            DeviceType = DeviceType.Mine;
+        }
+
+        public override bool CanUse()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string Use()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Silence
+    : Device
+    {
+        public Silence()
+        {
+            DeviceType = DeviceType.Silence;
+        }
+
+        public override bool CanUse()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string Use()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    #endregion
+
+    public class GameManager
+    {
+        public List<Player> Players { get; set; } = new List<Player> { new Player { PlayerType = PlayerType.Me }, new Player { PlayerType = PlayerType.Enemy } };
+        public Map Map { get; set; } = new Map();
+
+        /// <summary>
+        /// Le nombre de tour
+        /// </summary>
+        public int Counter { get; set; } = 0;
+
+        public Player Me => Players.First(p => p.PlayerType == PlayerType.Me);
+        public Player Enemy => Players.First(p => p.PlayerType == PlayerType.Enemy);
+
+        public MapCell MapCell(PlayerType playerType, Direction direction)
+        {
+            Player _player;
+            if (playerType == PlayerType.Me)
+                _player = Me;
+            else
+                _player = Enemy;
+
+            var xOffset = direction == Direction.West ? -1 : direction == Direction.Est ? 1 : 0;
+            var yOffset = direction == Direction.North ? -1 : direction == Direction.South ? 1 : 0;
+
+            
+
+            // On veut aller à l'ouest mais on est déjà au maximun ...
+            if (_player.Position.X == 0 && direction == Direction.West)
+                return null;
+
+            // On veut aller à l'est mais on est déjà au maximun ...
+            if (_player.Position.X == Map.Width - 1 && direction == Direction.Est)
+                return null;
+
+            // On veut aller au sud mais on est déjà au maximun ...
+            if (_player.Position.Y == Map.Height - 1 && direction == Direction.South)
+                return null;
+
+            // On veut aller au nord mais on est déjà au maximun ...
+            if (_player.Position.Y == 0 && direction == Direction.North)
+                return null;
+
+            var cell = Map.MapConfiguration[_player.Position.Y + yOffset][_player.Position.X + xOffset];
+            return cell;
+        }
+        public bool CanDoAction(MapCell mapCell, Direction direction)
+        {
+            if (mapCell == null)
+                return false;
+
+            if (mapCell.Visited)
+                return false;
+
+            if (mapCell.CellType != CellType.Empty)
+                return false;
+
+            // Je suppose que si je peux y aller je vais y aller
+            // A modifier plus tard
+            Console.Error.WriteLine($"current position : {Me.Position} - want to {direction.ToMove()}");
+            mapCell.Visited = true;
+            Me.LastMoves.Add(direction);
+            return true;
+        }
+
+        #region Ctor & Initialization
+        public GameManager()
+        {
+            var inputs = Helpers.ReadLine().Split(' ');
+            Map.Width = int.Parse(inputs[0]);
+            Map.Height = int.Parse(inputs[1]);
+            Me.PlayerId = int.Parse(inputs[2]);
+        }
+
+        public void InitializeMap()
+        {
+            for (int x = 0; x < Map.Height; x++)
+            {
+                var y = 0;
+                Map.MapConfiguration.Add(Helpers.ReadLine(debug: false).Select(c =>
+                {
+                    var cell = new MapCell
+                    {
+                        Cell = c,
+                        Position = new Position { X = x, Y = y }
+                    };
+                    y++;
+                    return cell;
+                }).ToList());
+            }
+
+#if WriteDebug
+            // Console.Error.WriteLine($"mapRow: {Map.MapConfiguration.Count}");
+#endif
+        }
+        public void SetPlayersInformations()
+        {
+            var inputs = Helpers.ReadLine().Split(' ');
+            Me.Position.X = int.Parse(inputs[0]);
+            Me.Position.Y = int.Parse(inputs[1]);
+
+            if (Counter > 0)
+            {
+                // Non disponible au 1er tours
+                Me.HealthPoint = int.Parse(inputs[2]);
+                Enemy.HealthPoint = int.Parse(inputs[3]);
+            }
+
+
+            if (inputs.Length >= 7)
+            {
+                Me.Torpedo.Couldown = int.Parse(inputs[4]);
+                Me.Sonar.Couldown = int.Parse(inputs[5]);
+                Me.Silence.Couldown = int.Parse(inputs[6]);
+                Me.Mine.Couldown = int.Parse(inputs[7]);
+            }
+
+            string sonarResult = Helpers.ReadLine();
+            string opponentOrders = Helpers.ReadLine();
+
+            // On enregistre le précédent déplacement
+            if (opponentOrders != "NA")
+                Enemy.LastMoves.Add(opponentOrders.ToDirection());
+
+        }
+        #endregion
+
+        public void WritePosition()
+        {
+            Console.Error.WriteLine($"My position is :{Me.Position}");
+            Console.Error.WriteLine($"Enemy position is :{Enemy.Position}");
+        }
+
+
+        public void Play()
+        {
+            string actions = "";
+
+            UseDevice(ref actions);
+            Move(ref actions);
+            LoadDevice(ref actions);
+
+            Counter++;
+            Console.WriteLine(actions);
+
+        }
+
+        /// <summary>
+        /// Attaquer
+        /// </summary>
+        private void UseDevice(ref string actions)
+        {
+            //if(Me.Torpedo.CanUse())
+            //{
+            //    var lastMoves = Me.LastMoves.Skip(Me.LastMoves.Count - 4).Take(3).ToList();
+            //    var lastEnemyMoves = Enemy.LastMoves.Skip(Me.LastMoves.Count - 4).Take(3).ToList();
+            //    if(lastMoves.SequenceEqual(lastEnemyMoves)) {
+            //        var action = Me.Torpedo.Use();
+            //        actions = string.Format($"{action}|", Me.Position.Y, Me.Position.X);
+            //    }
+            //}
+
+        }
+
+        /// <summary>
+        /// Déplacer
+        /// </summary>
+        private void Move(ref string actions)
+        {
+            // On regarde les positions proches
+            var west = MapCell(PlayerType.Me, Direction.West);
+            var est = MapCell(PlayerType.Me, Direction.Est);
+            var north = MapCell(PlayerType.Me, Direction.North);
+            var south = MapCell(PlayerType.Me, Direction.South);
+
+            // on regarde les quels sont accessibles et pour le moment on prend la première
+            if (CanDoAction(west, Direction.West))
+                actions = Direction.West.ToMove();
+            else if (CanDoAction(est, Direction.Est))
+                actions = Direction.Est.ToMove();
+            else if (CanDoAction(north, Direction.North))
+                actions = Direction.North.ToMove();
+            else if (CanDoAction(south, Direction.South))
+                actions = Direction.South.ToMove();
+            else
+            {
+                actions = "SURFACE";
+            }
+        }
+
+        /// <summary>
+        /// Charger
+        /// </summary>
+        private void LoadDevice(ref string actions)
+        {
+            //if (Me.Torpedo.Couldown > 0)
+            //    actions += " TORPEDO";
+
+            //if (Me.Sonar.Couldown < 0)
+            //    actions += " SONAR";
+        }
+
+
+
+    }
+
+
+
+    #region Common
+
+    #region Helpers
+    public static class Helpers
+    {
+        /// <summary>
+        /// Permet de lire la ligne et de retourner son contenu
+        /// Log si nécessaire l'information lue
+        /// </summary>
+        /// <param name="newLine"></param>
+        /// <param name="debug"></param>
+        /// <returns></returns>
+        public static string ReadLine(bool newLine = true, bool debug = true)
+        {
+            var msg = Console.ReadLine();
+            if (debug)
+            {
+                if (newLine)
+                    Console.Error.WriteLine(msg);
+                else
+                    Console.Error.Write(msg);
+            }
+
+            return msg;
+        }
+    }
+
+    public static class CharHelpers
+    {
+        /// <summary>
+        /// Permet de passer du caractère au type de cellule
+        /// </summary>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public static CellType ToCellType(this char c)
+        {
+            switch (c)
+            {
+                case 'x': return CellType.Island;
+                case '.': return CellType.Empty;
+                default: return CellType.Unknow;
+            }
+        }
+    }
+
+    public static class PositionHelpers
+    {
+        public static int ToSection(this Position position)
+        {
+            var section = 0;
+            if (position.X <= 4)
+                section = 1;
+            else if (position.X <= 9)
+                section = 2;
+            else
+                section = 3;
+
+            if (position.Y <= 4)
+                section = 1;
+            else if (position.Y <= 9)
+                section += 3;
+            else
+                section += 6;
+
+            return section;
+        }
+    }
+
+    public static class DirectionHelpers
+    {
+        public static string ToMove(this Direction direction)
+        {
+            switch (direction)
+            {
+                case Direction.South: return "MOVE S";
+                case Direction.North: return "MOVE N";
+                case Direction.West: return "MOVE W";
+                case Direction.Est: return "MOVE E";
+            }
+            return null;
+        }
+
+        public static Direction ToDirection(this string move)
+        {
+            switch (move)
+            {
+                case "MOVE S": return Direction.South;
+                case "MOVE N": return Direction.North;
+                case "MOVE W": return Direction.West;
+                case "MOVE E": return Direction.Est;
+            }
+            return Direction.None;
+        }
+    }
+    #endregion
+
+    #region Enums
+    public enum PlayerType
+    {
+        None = 0,
+        Me = 1,
+        Enemy = 2
+    }
+
+    public enum CellType
+    {
+        Unknow = 0,
+        Island = 1,
+        Empty = 2
+    }
+
+    public enum DeviceType
+    {
+        None = 0,
+        Torpedo = 1,
+        Sonar = 2,
+        Silence = 3,
+        Mine = 4
+    }
+
+    public enum Direction
+    {
+        None = 0,
+        West = 1,
+        Est = 2,
+        North = 3,
+        South = 4
+    }
+    #endregion
+
+    #endregion
+}
---
+++ OceanOfCode/Rules.md
@@ -0,0 +1,50 @@
+﻿#The Goal
+Vous pilotez un sous-marin et vous savez qu'un ennemi est présent près de vous parce que vous écoutez sa communication sur les fréquences radio.
+Vous ne savez pas exactement où il se trouve mais vous pouvez entendre tous ses ordres.
+Vous et votre adversaire avez 6 points d'impact. Lorsque les points de vie d'un joueur atteignent 0, le joueur perd.
+
+#Rules
+##Definitions
+
+- Les sous-marins se déplacent sur une carte constituée d'eau et d'îles.
+	Ils ne peuvent se déplacer que sur des cellules contenant de l'eau. Ils peuvent partager la même cellule sans entrer en collision.
+- La carte est composée de 15 cellules en largeur et de 15 cellules en hauteur.
+	Les coordonnées commencent à (0,0) qui est la cellule supérieure gauche de la carte.
+- La carte est divisée en 9 secteurs, qui contiennent chacun 25 cellules (5x5 blocs de cellules).
+Le secteur supérieur gauche est le 1, le secteur inférieur droit est le 9.
+
+##Beginning of the game
+Au début du jeu, vous recevrez une carte (15x15 cases) qui indique la position des îles.
+Les îles sont des obstacles.
+Vous ne pouvez pas vous déplacer ou tirer à travers les îles. Ensuite, vous déciderez où vous voulez placer votre sous-marin en indiquant une coordonnée (x,y).
+
+##Each turn
+Il s'agit d'un jeu au tour par tour, ce qui signifie que chaque joueur joue un tour après l'autre.
+Le joueur avec l'id 0 commence. Pendant votre tour, grâce à l'analyse des fréquences radio, vous recevrez une indication de ce que votre adversaire a fait.
+Par exemple, vous pouvez recevoir qu'il s'est déplacé vers le nord.
+C'est à vous d'utiliser cette précieuse information pour détecter où il se trouve.
+Ensuite, vous devez effectuer au moins une action.
+
+##Actions
+À chaque tour, vous devez effectuer au moins une action.
+Vous pouvez effectuer plusieurs actions en les enchaînant à l'aide du tuyau |.
+Mais vous ne pouvez utiliser chaque type d'action qu'une seule fois par tour (vous ne pouvez vous déplacer qu'une seule fois par tour, pas plus).
+Si vous ne parvenez pas à produire une action valide, vous ferez surface à ce tour.
+
+##Move
+Une action de déplacement déplace votre sous-marin 1 cellule dans une direction donnée (nord, est, sud, ouest) et charge une puissance de votre choix.
+Lorsque vous vous déplacez, vous devez respecter les règles suivantes :
+- Vous ne pouvez pas vous déplacer à travers les îles
+- Vous ne pouvez pas vous déplacer sur une cellule que vous avez déjà visitée auparavant
+
+Vous pouvez décider de ce que vous voulez charger.
+Différents appareils nécessitent un montant différent de frais pour être prêts. Dans cette ligue, vous ne pouvez charger que la torpille.
+
+##Surface
+En utilisant la surface, vous réinitialiserez le cheminement des cellules visitées afin de pouvoir vous déplacer librement vers une cellule que vous avez déjà visitée.
+Mais le surfacing a un impact majeur : votre adversaire saura dans quel secteur vous faites surface et vous perdrez 1 point de vie.
+
+##Torpedo
+Une torpille nécessite 3 actions de charge pour être prête. Lorsqu'elle est complètement chargée, la torpille peut être tirée à une position arbitraire dans l'eau, dans un rayon de 4 cellules.
+Cela permet à la torpille de contenir les coins et de contourner les îles, mais pas de les traverser.
+Les dégâts de l'explosion sont de 2 sur la cellule elle-même et de 1 sur tous les voisins (y compris les diagonales).
