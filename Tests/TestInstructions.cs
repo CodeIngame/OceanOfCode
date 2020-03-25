4bf2797ded5252a051598614dd72d1d884e32c7f
Thomas GICQUEL <m.gicquel.thomas@gmail.com>
2020-03-25 21:19:59 +0100

    Changement de ligue (#3)
    * Refactoring surface & toInstructions
    
    * Changement de league :D

---
+++ Tests/TestInstructions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tests
+{
+    public class TestInstructions
+    {
+
+        public List<string> Seeds { get; set; } = new List<string> { "MOVE N TORPEDO", "MOVE E| TORPEDO 8 6", "MOVE E| TORPEDO 8 6", "TORPEDO 0 8|MOVE E TORPEDO", "SURFACE 5 | MOVE W", "SURFACE 7" };
+
+
+        public void Handle() {
+
+            foreach(var s in Seeds)
+            {
+                var cmd = s;
+                var isMultiOrders = cmd.Contains("|");
+
+                foreach(var order in cmd.Split('|', StringSplitOptions.RemoveEmptyEntries))
+                {
+
+                }
+
+              
+            }
+
+
+        }
+    }
+
+    public enum DeviceType
+    {
+        None = 0,
+        Torpedo = 1,
+        Sonar = 2,
+        Silence = 3,
+        Mine = 4
+    }
+}
